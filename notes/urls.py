c6215e8fb680637ea7cec446ac84870f9ac238ae
Kylie Williamson <kyliewill@rocketmail.com>
2017-04-30 09:17:33 +0000

    http requests working

--- notes/urls.py
+++ notes/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name = 'notes'
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),    
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^delete/(?P<note_id>[0-9]+)/$', views.delete, name='delete'),
+    url(r'^edit/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='edit'),
+    url(r'^add/add/$', views.add, name='add'),
+    url(r'^submitChange/(?P<pk>[0-9]+)/$', views.change, name='change')
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Note
-
 
 class IndexView(generic.ListView):
     template_name = 'notes/index.html'
@@ -11,8 +12,26 @@ class IndexView(generic.ListView):
     
     def get_queryset(self):
         return Note.objects.order_by('-pub_date').all()
-        
+
+class DetailView(generic.DetailView):
+    model = Note
+    template_name = 'notes/edit.html'
+    
+    
 def delete(request, note_id):
-    note = get_object_or_404(Note, pk=note_id)
-    selected_note = note.choice_set.get(pk=request.POST['note'])
-    selected_note.delete();
+    note = get_object_or_404(Note, pk=note_id).delete()
+    return HttpResponseRedirect(reverse('notes:index'))
+    
+def add(request):
+    title = request.POST.get("title", "")
+    content = request.POST.get("content", "")
+    newNote = Note(note_title = title, note_content=content, pub_date = timezone.now())
+    newNote.save()
+    return HttpResponseRedirect(reverse('notes:index'))
+    
+def change(request, pk):
+    note = get_object_or_404(Note, pk=pk)
+    note.note_title = request.POST.get("title", "")
+    note.note_content = request.POST.get("content", "")
+    note.save()
+    return HttpResponseRedirect(reverse('notes:index'))
